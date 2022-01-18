c272adcb9acf69f6bcfc93ff253ad892e2193666
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-01-18 11:53:53 -0500

    API: * Fix generic type in VEBTree * Add nullability to min and max in VEBTree * Add lookup, findNext, and findPrev to VEBTree

--- src/commonMain/kotlin/com/github/dk96/VEBTree.kt
+++ src/commonMain/kotlin/com/github/dk96/VEBTree.kt
@@ -1,12 +1,12 @@
 package com.github.dk96
 
-interface VEBTree<IntType : Comparable<*>> {
+interface VEBTree<IntType : Comparable<IntType>> {
 
     /** The smallest value present in the Tree */
-    val min: IntType
+    val min: IntType?
 
     /** The greatest value present in the Tree */
-    val max: IntType
+    val max: IntType?
 
     /** Insert this value into the Tree if not present */
     fun insert(value: IntType)
@@ -16,5 +16,14 @@ interface VEBTree<IntType : Comparable<*>> {
 
     /** Obtain the child tree at the given index */
     fun getChildAt(index: Int): VEBTree<IntType>?
+
+    fun lookup(value: IntType)
+        : Boolean = VEBOperations.lookup(this, value)
+
+    fun findNext(value: IntType)
+        : IntType? = VEBOperations.findNext(this, value)
+
+    fun findPrev(value: IntType)
+        : IntType? = VEBOperations.findPrev(this, value)
 
 }
