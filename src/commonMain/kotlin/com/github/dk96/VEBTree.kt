f806b2c97e938b13a8dfa5d14b6302b696c7f615
DK96 <69859316+DK96-OS@users.noreply.github.com>
2022-02-06 10:05:05 -0500

    VEBTree Abstraction: * Implement insert, delete, and lookup methods at the node level * Create insertSubtree, deleteFromSubtree, and lookInSubtree abstract methods that handle stepping between nodes * Fix summary member, use BooleanArray * Add children HashMap member * Let min, max be declared in abstract class

--- src/commonMain/kotlin/com/github/dk96/VEBTree.kt
+++ src/commonMain/kotlin/com/github/dk96/VEBTree.kt
@@ -4,6 +4,8 @@ import kotlin.math.log2
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
+/** A van Emde Boas Tree Data Structure
+ * Developed by DK96-OS : 2022 */
 abstract class VEBTree<IntType : Comparable<IntType>>(
     val size: Int
 ) {
@@ -24,22 +26,28 @@ abstract class VEBTree<IntType : Comparable<IntType>>(
 
     /** The smallest value present in the Tree
      * This value is not stored anywhere else */
-    abstract val min: IntType?
+    var min: IntType? = null
+        protected set
 
     /** The greatest value present in the Tree */
-    abstract val max: IntType?
+    var max: IntType? = null
+        protected set
 
-    protected var summary
-        : ByteArray? = null
+    protected var mSummary
+        : BooleanArray? = null
 
-    /** Insert this value into the Tree if not present */
-    abstract fun insert(value: IntType)
+    /** Child Nodes / Sub trees */
+    protected var mChildren
+        : HashMap<Int, VEBTree<IntType>>? = null
+
+    /** Insert this value into a subtree */
+    abstract fun insertSubtree(value: IntType)
 
     /** Delete this value from the Tree if present */
-    abstract fun delete(value: IntType)
+    abstract fun deleteFromSubtree(value: IntType)
 
     /** Determine whether this value is present */
-    abstract fun lookup(value: IntType)
+    abstract fun lookInSubtree(value: IntType)
         : Boolean
 
     /** Find the successor for this value
@@ -51,5 +59,51 @@ abstract class VEBTree<IntType : Comparable<IntType>>(
      * @return Predecessor or null if none */
     abstract fun predecessor(value: IntType)
         : IntType?
+
+    /** Insert this value into the Tree if not present */
+    fun insert(value: IntType) {
+        val min = min
+        when {
+            min == null -> {
+                this.min = value
+                this.max = value
+            }
+            min > value -> {
+                this.min = value
+                insert(min)
+            }
+            min == value -> {}
+            else -> insertSubtree(value)
+        }
+    }
+
+    /** Delete a value from this tree
+     * @return True if this tree is now empty, can be deleted */
+    fun delete(value: IntType): Boolean {
+        val min = min
+        when {
+            min == null -> return true
+            value == min -> {
+                val successor = successor(value)
+                if (successor != null) {
+                    delete(successor)
+                    this.min = successor
+                } else {
+                    this.min = null
+                    return true
+                }
+            }
+            else -> deleteFromSubtree(value)
+        }
+        return false
+    }
+
+    /** Determine whether this value is present */
+    fun lookup(value: IntType)
+        : Boolean = when (min) {
+        null -> false
+        value -> true
+        else -> lookInSubtree(value)
+    }
 
 }
